f1dde3ceb8ea9831c03dbd31bdc8160ab325846a
Inoue Hikari <inoue@aizulab.com>
2026-07-23 11:03:28 +0900

    Add redirect from root URL to /reservations/

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("apps.api.urls")),
     path("reservations/", include("apps.reservations.urls")),
-    path("", include("apps.web.urls")),
+    path("", RedirectView.as_view(url="/reservations/", permanent=False)),
 ]
